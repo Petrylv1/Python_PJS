2568b32e9b2108761545c6ccf9ace941cbb3bbbf
Petryl <62471248+Petrylv1@users.noreply.github.com>
2023-01-08 23:45:22 +0100

    Add files via upload

---
+++ Lista1.py
@@ -0,0 +1,21 @@
+print("Lista 1 zad 1")
+print("==========================================")
+print("Kalkulator objętości i pól powierzni brył")
+print("==========================================")
+print("Proszę wybrać bryłę:")
+print("a - Kula")
+print("b - Prostopadłościan")
+print("c - Stożek")
+
+wybor=str(input("Wybór: "))
+
+if(wybor=='a'):
+    print("Wybrałeś kulę")
+    print("Wybrałeś kulę")
+    print("Wybrałeś kulę")
+if(wybor=='b'):
+    print("Wybrałeś Prostopadłościan")
+if(wybor=='c'):
+    print("Wybrałeś Stożek")
+    print("Podaj 1")
+    print("Podaj 2:")
