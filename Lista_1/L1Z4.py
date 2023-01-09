ed375549d1dd05017b2f648904acbec6f9f1cc51
Petryl <62471248+Petrylv1@users.noreply.github.com>
2023-01-09 01:35:02 +0100

    Lista 1 Zad 4
    Zabawne zadanie ale powinno być do ogarnięcia.
    Nie to co gwiazdki i inne tego typu.

---
+++ Lista_1/L1Z4.py
@@ -0,0 +1,8 @@
+print("Lista 1 zad 4")
+print("================")
+print("Ciągi znaków")
+print("=================")
+ciag=str(input("Podaj ciąg znaków:"))
+
+wyrazowo = ciag.split()
+print("Wszystkie wyrazy w zdaniu: {}",wyrazowo)
