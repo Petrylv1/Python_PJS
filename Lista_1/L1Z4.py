566d31e07a94bcc7cf70642eb5ed45b5b631a829
Petryl <62471248+Petrylv1@users.noreply.github.com>
2023-01-17 03:18:36 +0100

    Działa. Nie wiadomo czy dokładnie jak chciał. idc
    Zdania fest.

--- Lista_1/L1Z4.py
+++ Lista_1/L1Z4.py
@@ -2,7 +2,17 @@ print("Lista 1 zad 4")
 print("================")
 print("Ciągi znaków")
 print("=================")
-ciag=str(input("Podaj ciąg znaków:"))
 
-wyrazowo = ciag.split()
-print("Wszystkie wyrazy w zdaniu: {}",wyrazowo)
+sentence = input("Podaj zdanie: ")
+
+print("Liczba wszystkich liter: ", len(sentence))
+
+print("Liczba liter bez spacji: ", len(sentence.replace(" ", "")))
+exclude_char = input("Podaj znak do pominięcia: ")
+print("Liczba liter bez podanego znaku: ", len(sentence.replace(exclude_char, "")))
+
+print("Wyrazy w zdaniu: ", sentence.split())
+
+split_char = input("Podaj znak do podziału zdania: ")
+sentence_parts = sentence.split(split_char)
+print("Części zdania po podziale: ", sentence_parts)
