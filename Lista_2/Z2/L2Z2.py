865eed52f2481607e1defe9f3c7fa52b770bcc5c
Petryl <62471248+Petrylv1@users.noreply.github.com>
2023-01-24 06:13:25 +0100

    Całe zadanie 2. Działa ale znaki pl not works

---
+++ Lista_2/Z2/L2Z2.py
@@ -0,0 +1,33 @@
+import csv
+import random
+
+def generate_random_data(quantity):
+    data = []
+    with open('imiona.txt', encoding="utf-8") as f:
+        imiona = f.read().splitlines()
+    with open('nazwiska.txt', encoding="utf-8") as f:
+        nazwiska = f.read().splitlines()
+    with open('ulice.txt', encoding="utf-8") as f:
+        ulice = f.read().splitlines()
+    with open('miasta.txt', encoding="utf-8") as f:
+        miasta = f.read().splitlines()
+    with open('kraje.txt', encoding="utf-8") as f:
+        kraje = f.read().splitlines()
+    for i in range(quantity):
+        imie = random.choice(imiona)
+        nazwisko = random.choice(nazwiska)
+        pesel = ''.join([str(random.randint(0, 9)) for _ in range(11)])
+        ulica = random.choice(ulice)
+        nr_domu = str(random.randint(1, 50))
+        miasto = random.choice(miasta)
+        kraj = random.choice(kraje)
+        data.append([imie, nazwisko, pesel, ulica, nr_domu, miasto, kraj])
+    # zapisanie danych w pliku csv
+    with open('losowe_dane.csv', 'w', newline='', encoding="utf-8") as f:
+        writer = csv.writer(f, delimiter=';')
+        writer.writerow(['imie', 'nazwisko', 'pesel', 'ulica', 'nr_domu', 'miasto', 'kraj'])
+        writer.writerows(data)
+
+# wywołanie funkcji z ilością danych podaną przez użytkownika
+quantity = int(input("Podaj ilość danych, które chcesz wygenerować: "))
+generate_random_data(quantity)
